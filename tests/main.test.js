c47b0505713056523ac2af117cbf8176563595b8
vital987321 <vitaliyvelichko@gmail.com>
2024-10-14 22:03:18 +0200

    Added ExpandableText.test, ToastDemo.test, Greet.test

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,7 +1,17 @@
 import { it, expect, describe } from 'vitest'
+import { db } from './mocks/db'
 
 describe('group', () => {
     it('should', () => {
         expect(1).toBeTruthy()
+    })
+    it('should', async () => {
+        const firstProduct = db.product.create()
+        console.log(firstProduct)
+        const product = db.product.create({ name: 'Apple' });
+        console.log(db.product.getAll())
+        db.product.delete({ where: { id: { equals: product.id } } })
+        db.product.delete({where: {}});
+        
     })
 })
--- tests/setupTest.ts
+++ tests/setupTest.ts
@@ -1,1 +1,28 @@
-import '@testing-library/jest-dom/vitest';
+import "@testing-library/jest-dom/vitest";
+import ResizeObserver from "resize-observer-polyfill";
+// import { server } from "./src/tests/mocks/server";
+// import { afterAll, beforeAll } from "vitest";
+
+// beforeAll(() => server.listen());
+// afterEach(() => server.resetHandlers());
+// afterAll(()=>server.close())
+
+global.ResizeObserver = ResizeObserver;
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
+window.HTMLElement.prototype.hasPointerCapture = vi.fn();
+window.HTMLElement.prototype.releasePointerCapture = vi.fn();
